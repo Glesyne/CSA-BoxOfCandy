ff27630c41877794427c02e756aa86c5651d5eff
Glesyne <alexc161@nycstudents.net>
2025-05-05 04:27:39 +0000

    Final Version

---
+++ Candy.java
@@ -0,0 +1,17 @@
+public class Candy {
+
+    private String flavor; 
+    
+    public Candy(String flavors){
+        flavor = flavors; 
+    }
+
+    public String getFlavor() {
+        return flavor; 
+    }
+
+    public String toString(){
+        return flavor; 
+    }
+
+}
